0b4533f89f9b95ac5f48d55ec68424bad8519dc4
Sarfaraz Hussain <sarfarazsajjad@gmail.com>
2019-11-02 13:33:15 +0500

    class functions

--- classes.py
+++ classes.py
@@ -1,5 +1,6 @@
 class MyClass:
     pass
+
 oa = MyClass()
 ob = MyClass()
 
@@ -11,8 +12,16 @@ print(ob)
 class Point:
     pass
 
+    def myfunction(self,a: int):
+        'this function prints a number that it takes as argument'
+        print(a)
+
 p1 = Point()
 p1.x = 1
 p1.y = 2
+p1.myfunction(1)
+print(p1.myfunction.__annotations__)
+print(p1.myfunction.__doc__)
+
 
 print(p1.x,p1.y)
